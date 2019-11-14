a05ebe6710e5bbca4322878391fdd7d2be8a8656
nurquhart <n.urquhart@napier.ac.uk>
2019-11-14 15:33:38 +0000

    Updated DrawProblem.java
    Added more figures.

--- Chapter2/src/book/ch2/DrawProblem.java
+++ Chapter2/src/book/ch2/DrawProblem.java
@@ -20,12 +20,53 @@ public class DrawProblem {
 	private static int SCALE =2;
 	public static void main(String[] args) {
 		VRPProblemFactory factory = new VRPProblemFactory();
-		CVRPProblem problem = factory.buildProblem("./data/P-n76-k4.vrp");
-		//NearestNTSPSolver nn = new NearestNTSPSolver();
-		//((TSPProblem)problem).solve(nn);
-		//problem.solve(new GrandTour());
-		//problem.solve(new ClarkeWright());
-		writeSVG(problem,"P-n76-k4.svg");
+		CVRPProblem problem = factory.buildProblem("./data/A-n65-k9.vrp");
+		NearestNTSPSolver nn = new NearestNTSPSolver();
+		((TSPProblem)problem).solve(nn);
+		writeSVG((TSPProblem)problem,"P-n76-k4.GT.svg");
+		problem.solve(new GrandTour());
+		writeSVG(problem,"P-n76-k4.GT-final.svg");
+		problem.solve(new ClarkeWright());
+		writeSVG(problem,"P-n76-k4.CW-final.svg");
+		
+		
+	}
+	
+	private static void writeSVG(TSPProblem problem, String fName) {
+		System.out.println();
+		String svg = "";
+		svg = "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"200\" height=\"200\"> \n";
+		Visit v  = problem.getStart();
+		svg = svg + "<rect x=\" "+(v.x*SCALE)+" \" y=\" "+(v.y*SCALE)+" \" height=\"8\" width=\"8\" fill=\"red\"/>\n";
+		
+		for (Object o : problem.getSolution()) {
+			v = (Visit) o;
+			svg = svg + "<circle cx=\" "+(v.x*SCALE)+" \" cy=\" "+(v.y*SCALE)+" \" r=\"2\" fill=\"black\"/>\n";
+		}
+		
+		if (problem.getSolution()!= null) {
+				Visit previous = problem.getStart();
+				for (Visit current : problem.getSolution()) {
+						svg = svg + "<line x1=\" "+(previous.x*SCALE)+" \" y1=\" "+(previous.y*SCALE)+" \" x2=\" "+(current.x*SCALE)+" \" y2=\" "+(current.y*SCALE)+" \" style=\"stroke:rgb(0,0,0);stroke-width:1\" />\n" ;
+						previous = current;
+				}
+				svg = svg +"<line x1=\" "+(previous.x*SCALE)+" \" y1=\" "+(previous.y*SCALE)+" \" x2=\" "+(problem.getStart().x*SCALE)+" \" y2=\" "+(problem.getStart().y*SCALE)+" \" style=\"stroke:rgb(0,0,0);stroke-width:1\" />\n" ;
+			
+		}
+		svg = svg +"</svg>";
+		//write to file
+		
+		
+        try (FileWriter writer = new FileWriter(fName );
+              BufferedWriter bw = new BufferedWriter(writer)) {
+
+            bw.write(svg);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+		
 	}
 	
 	private static void writeSVG(CVRPProblem problem, String fName) {
