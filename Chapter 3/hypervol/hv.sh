0e7041dbe84ddca96840f24cb9807c97952ce15c
neilurquhart <n.urquhart@napier.ac.uk>
2020-01-23 22:13:41 +0000

    Chapter 3 - first draft done!

---
+++ Chapter 3/hypervol/hv.sh
@@ -0,0 +1,288 @@
+echo 'blon-1--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvblon-1--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-1--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-1--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-1--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvblon-1--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-1--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvblon-1--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'blon-1--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvblon-1--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-1--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-1--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-2--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvblon-2--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-2--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-2--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-2--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvblon-2--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-2--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvblon-2--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'blon-2--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvblon-2--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-2--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-2--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-4--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvblon-4--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-4--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-4--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-4--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvblon-4--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-4--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvblon-4--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'blon-4--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvblon-4--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-4--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-4--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-8--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvblon-8--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-8--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-8--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-8--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvblon-8--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-8--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvblon-8--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'blon-8--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvblon-8--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-8--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-8--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-r00--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvblon-r00--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-r00--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-r00--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-r00--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvblon-r00--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-r00--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvblon-r00--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'blon-r00--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvblon-r00--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-r00--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-r00--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-rnd--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvblon-rnd--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-rnd--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-rnd--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-rnd--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvblon-rnd--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-rnd--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvblon-rnd--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'blon-rnd--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvblon-rnd--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'blon-rnd--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvblon-rnd--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-1--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-1--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-1--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-1--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-1--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvcluster-1--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-1--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-1--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'cluster-1--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvcluster-1--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-1--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-1--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-2--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-2--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-2--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-2--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-2--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvcluster-2--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-2--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-2--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'cluster-2--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvcluster-2--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-2--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-2--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-4--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-4--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-4--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-4--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-4--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvcluster-4--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-4--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-4--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'cluster-4--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvcluster-4--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-4--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-4--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-8--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-8--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-8--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-8--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-8--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvcluster-8--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-8--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-8--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'cluster-8--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvcluster-8--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-8--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-8--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-r00--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-r00--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-r00--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-r00--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-r00--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvcluster-r00--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-r00--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-r00--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'cluster-r00--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvcluster-r00--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-r00--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-r00--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-rnd--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-rnd--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-rnd--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-rnd--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-rnd--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvcluster-rnd--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-rnd--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvcluster-rnd--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'cluster-rnd--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvcluster-rnd--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'cluster-rnd--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvcluster-rnd--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-1--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvlon-1--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-1--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-1--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-1--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvlon-1--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-1--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvlon-1--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'lon-1--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvlon-1--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-1--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-1--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-2--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvlon-2--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-2--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-2--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-2--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvlon-2--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-2--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvlon-2--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'lon-2--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvlon-2--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-2--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-2--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-4--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvlon-4--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-4--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-4--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-4--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvlon-4--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-4--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvlon-4--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'lon-4--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvlon-4--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-4--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-4--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-8--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvlon-8--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-8--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-8--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-8--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvlon-8--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-8--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvlon-8--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'lon-8--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvlon-8--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-8--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-8--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-r00--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvlon-r00--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-r00--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-r00--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-r00--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvlon-r00--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-r00--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvlon-r00--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'lon-r00--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvlon-r00--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-r00--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-r00--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-rnd--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvlon-rnd--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-rnd--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-rnd--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-rnd--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvlon-rnd--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-rnd--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvlon-rnd--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'lon-rnd--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvlon-rnd--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'lon-rnd--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvlon-rnd--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-1--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-1--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-1--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-1--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-1--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvoffset-1--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-1--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-1--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'offset-1--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvoffset-1--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-1--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-1--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-2--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-2--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-2--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-2--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-2--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvoffset-2--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-2--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-2--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'offset-2--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvoffset-2--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-2--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-2--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-4--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-4--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-4--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-4--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-4--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvoffset-4--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-4--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-4--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'offset-4--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvoffset-4--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-4--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-4--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-8--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-8--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-8--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-8--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-8--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvoffset-8--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-8--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-8--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'offset-8--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvoffset-8--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-8--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-8--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-r00--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-r00--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-r00--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-r00--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-r00--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvoffset-r00--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-r00--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-r00--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'offset-r00--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvoffset-r00--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-r00--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-r00--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-rnd--useCO2_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-rnd--useCO2_CarPC.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-rnd--useCO2_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-rnd--useCO2_StaffCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-rnd--useCO2_TotalCost.res.csv' >> hyper.dat
+./hv hvoffset-rnd--useCO2_TotalCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-rnd--useCO2_TotalCost_CarPC.res.csv' >> hyper.dat
+./hv hvoffset-rnd--useCO2_TotalCost_CarPC.res.csv -r  '1 1 1' >> hyper.dat
+echo 'offset-rnd--useCO2_TravelCost.res.csv' >> hyper.dat
+./hv hvoffset-rnd--useCO2_TravelCost.res.csv -r  '1 1' >> hyper.dat
+echo 'offset-rnd--useTravelCost_StaffCost.res.csv' >> hyper.dat
+./hv hvoffset-rnd--useTravelCost_StaffCost.res.csv -r  '1 1' >> hyper.dat
--- Chapter 3/src/book/ch3/rep2/NonDominatedPop.java
+++ Chapter 3/src/book/ch3/rep2/NonDominatedPop.java
@@ -18,27 +18,64 @@ public class NonDominatedPop extends ArrayList<Domination> {
 	private RandomSingleton rnd = RandomSingleton.getInstance();
 	//Note that we use the RandomSingleton object to generate random numbers
 
-	public NonDominatedPop() {//Defualt constructor.
-		super();
-	}
-			
+	public NonDominatedPop() {super();}//Default constructor.
+
 	public NonDominatedPop(int size, CVRPProblem theProblem) {
 		//Create a new population with <size> random solutions to <theProblem>
 		//Initialise population with random solutions
 		BiObjectiveIndividual best = null;
 		for (int count=0; count < size; count++){
 			BiObjectiveIndividual i = new BiObjectiveIndividual(theProblem);
-
 			if (best == null) 
 				best = i;
 			if (i.evaluate() < best.evaluate()) 
 				best = i;
 			this.add(i);
-			
 		}
 	}
 
-	
+	public Domination getDominator(){
+		/*
+		 * Select two individuals at random and if one dominates the other return it
+		 */
+		Domination x = this.get(rnd.getRnd().nextInt(this.size()));
+		Domination y = this.get(rnd.getRnd().nextInt(this.size()));
+		if (x.dominates(y))
+			return x;
+		else
+			return y;
+	}
+
+	public NonDominatedPop extractNonDom(){
+		/*
+		 * Return a population containing ONLY the non dominated individuals
+		 */
+		NonDominatedPop result = new NonDominatedPop();
+		for (Domination current : this){
+			boolean dominated =  false;
+			for (Domination member : this){
+				if (member.dominates(current))
+					dominated = true;
+			}
+			if (! dominated)
+				result.add(current);
+		}
+		return result;
+	}
+
+	public NonDominatedPop removeNonDom(){
+		/*
+		 * Return a population containing ONLY the non dominated individuals
+		 * AND remove them from the current population
+		 */
+
+		NonDominatedPop nd = this.extractNonDom();
+		for (Object b: nd){
+			this.remove(b);
+		}
+		return nd;
+	}	
+
 	public String getStats(){
 		/*
 		 * Print out population statistics.
@@ -46,15 +83,15 @@ public class NonDominatedPop extends ArrayList<Domination> {
 		String res = "";
 		res = "n," + this.size();
 		int size = this.get(0).getVector().length;
-		
+
 		double[] max = new double[size];
 		double[] min = new double[size];
-		
+
 		for (int c=0; c < size; c++){
 			max[c] = -1;
 			min[c] = Double.MAX_VALUE;
 		}
-		
+
 		//Find the maximum and minimum for each objective
 		for(Domination d : this){
 			double[] v = d.getVector();
@@ -65,99 +102,10 @@ public class NonDominatedPop extends ArrayList<Domination> {
 					max[pos] = v[pos];
 			}
 		}
-		
+
 		for (int pos = 0 ; pos < size; pos++){
 			res = res + ",obj"+pos +"Min," + min[pos] + ",obj"+pos +"Max,"+ max[pos];
 		}
 		return res;
 	}
-	
-	public NonDominatedPop extractNonDom(){
-		/*
-		 * Return a population containing ONLY the non dominated individuals
-		 */
-		
-		NonDominatedPop result = new NonDominatedPop();
-		
-		for (Domination current : this){
-			boolean dominated =  false;
-			for (Domination member : this){
-			  if (member.dominates(current))
-				  dominated = true;
-			  }
-			
-			if (! dominated)
-				result.add(current);
-			}
-		return result;
-		}
-		
-	public NonDominatedPop removeNonDom(){
-		/*
-		 * Return a population containing ONLY the non dominated individuals
-		 * AND remove them from the current population
-		 */
-	
-		NonDominatedPop nd = this.extractNonDom();
-		for (Object b: nd){
-			this.remove(b);
-		}
-		return nd;
-	}	
-	
-//	public void delete(Domination d){
-//		int index=0;
-//		for (Domination current: this){
-//			if ((current.getVector()[0]==d.getVector()[0]) &&
-//					(current.getVector()[1]==d.getVector()[1])){
-//				;
-//				break;
-//			}
-//			index ++;
-//		}
-//		this.remove(index);
-//	}
-
-	public Domination getDominator(){
-		/*
-		 * Select two individuals at random and if one dominates the other 
-		 */
-		Domination x = this.get(rnd.getRnd().nextInt(this.size()));
-		Domination y = this.get(rnd.getRnd().nextInt(this.size()));
-		if (x.dominates(y))
-			return x;
-		else
-			return y;
-	}
-	public Domination getRIP(){
-		/*
-		 * Select 2 individuals at random.
-		 * If one dominates the other return the dominated individual
-		 */
-		while(true){
-			Domination x = this.get(rnd.getRnd().nextInt(this.size()));
-			Domination y = this.get(rnd.getRnd().nextInt(this.size()));
-
-			double[] xv = x.getVector();
-			double[] yv = y.getVector();
-
-			if ((xv[0] <= yv[0]))
-				if (xv[1] <= yv[1])
-					return y;
-		}
-	}
-	
-//	public Domination getDominated(){
-//		/*
-//		 * Select 2 individuals at random.
-//		 * If one dominates the other return the dominated individual
-//		 */
-//		Domination x = this.get(rnd.getRnd().nextInt(this.size()));
-//		Domination y = this.get(rnd.getRnd().nextInt(this.size()));
-//		
-//		if (y.dominates(x))
-//			return x;
-//		else
-//			return y;
-//	}
 }
