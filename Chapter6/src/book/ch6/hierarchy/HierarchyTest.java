d7a3f695abf33ea346f0798ea7c0808cc0bf9d0b
neilurquhart <n.urquhart@napier.ac.uk>
2020-03-22 21:14:47 +0000

    Updated CH6 results

--- Chapter6/src/book/ch6/hierarchy/HierarchyTest.java
+++ Chapter6/src/book/ch6/hierarchy/HierarchyTest.java
@@ -3,7 +3,18 @@ package book.ch6.hierarchy;
 public class HierarchyTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		// Create MWay
+		// Create Edin
+		// Create Glas
+		
+		//Identify link points Edin-Mawy
+		//Identify link points Glas-Mway
+		
+		//Test route
+		
+		
 
 	}
 
