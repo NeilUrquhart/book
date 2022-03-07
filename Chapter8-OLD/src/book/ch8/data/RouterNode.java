f29cc91a39545747ecf2b07edb9f24691e6bc2ab
Neil Urquhart <n.urquhart@napier.ac.uk>
2022-03-07 15:55:35 +0000

    Tidying up project - CH8

---
+++ Chapter8-OLD/src/book/ch8/data/RouterNode.java
@@ -0,0 +1,49 @@
+package book.ch8.data;
+
+import info.pavie.basicosmparser.model.Node;
+
+import java.util.ArrayList;
+
+import book.ch1.LatLon;
+
+public class RouterNode {
+	private int index;
+	private Node osmNode;
+	private boolean uplink;//Used for multi-layer graphs
+	
+	public RouterNode(Node aNode){
+		osmNode = aNode;
+	}
+
+	public void setUplink(){
+		uplink = true;
+	}
+	
+	public boolean isUplink(){
+		return uplink;
+	}
+	
+	public int getIndex(){
+		return index;
+	}
+	public void setIndex(int indx){
+		this.index = indx;
+	}
+	
+	public long getId(){
+		return Long.parseLong(this.osmNode.getId().substring(1));
+	}
+
+	public LatLon getLocation(){
+		LatLon location = new LatLon(this.osmNode.getLat(), this.osmNode.getLon());
+		return location;
+	}
+	
+	public double getDist(RouterNode other){
+		return this.getLocation().getDist(other.getLocation());
+	}
+
+	public String toString(){
+		return this.getId()+ "(" + this.getLocation()+")";
+	}	
+}
--- Chapter9/src/book/ch9/Cache.java
+++ Chapter9/src/book/ch9/Cache.java
@@ -1,6 +1,6 @@
 package book.ch9;
 
-import java.awt.geom.Point2D;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
